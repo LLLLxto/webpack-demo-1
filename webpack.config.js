c4a92e80d1da388ef04a4f37d24efa81fa0f10f2
Tong <LLLLxto@163.com>
2021-08-03 22:56:56 +0800

    update html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[contenthash].js'
-    }
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My App',
+      template: 'src/assets/test.html'
+    })
+  ]
 }
